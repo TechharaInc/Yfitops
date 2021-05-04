ddb5ed8f64a150ebed1c38377d879e0ead0203b5
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2021-05-04 22:58:33 +0900

    再生とスキップ

--- main.go
+++ main.go
@@ -17,7 +17,11 @@ import (
 	"github.com/zmb3/spotify"
 )
 
+const REACTION_SUCCEED = "✅"
+const REACTION_FAILED = "❌"
+
 var auth spotify.Authenticator
+var spotifyClient *spotify.Client
 
 var (
 	ch = make(chan *spotify.Client)
@@ -29,7 +33,7 @@ func main() {
 		log.Fatal("AAGH .env COULD NOT LOADED!")
 	}
 
-	auth = spotify.NewAuthenticator(os.Getenv("REDIRECT_URL"), spotify.ScopeUserReadPrivate)
+	auth = spotify.NewAuthenticator(os.Getenv("REDIRECT_URL"), spotify.ScopeUserReadPrivate, spotify.ScopeUserReadPlaybackState, spotify.ScopeUserModifyPlaybackState)
 	auth.SetAuthInfo(os.Getenv("SPOTIFY_CLIENT_ID"), os.Getenv("SPOTIFY_CLIENT_SECRET"))
 
 	http.HandleFunc("/callback", completeAuth)
@@ -52,21 +56,13 @@ func main() {
 
 	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
 
+	spotifyClient = <-ch
+
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, syscall.SIGTERM)
 	<-sc
 
 	dg.Close()
-}
-
-func awaitCompleteAuth() {
-	client := <-ch
-
-	user, err := client.CurrentUser()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("WE ARE LOGGED IN AS:", user.DisplayName)
 }
 
 func completeAuth(w http.ResponseWriter, r *http.Request) {
@@ -86,11 +82,13 @@ func completeAuth(w http.ResponseWriter, r *http.Request) {
 }
 
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-	if m.Author.ID == s.State.User.ID || m.Author.Bot {
+	prefix := os.Getenv("DISCORD_PREFIX")
+
+	if m.Author.ID == s.State.User.ID || m.Author.Bot || !strings.HasPrefix(m.Content, prefix) {
 		return
 	}
 
-	cmd := strings.Replace(m.Content, os.Getenv("DISCORD_PREFIX"), "", 1)
+	cmd := strings.Replace(m.Content, prefix, "", 1)
 	args := strings.Split(cmd, " ")
 
 	if strings.HasPrefix(cmd, "session") {
@@ -99,6 +97,55 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 			url := auth.AuthURL(os.Getenv("SPOTIFY_OAUTH_STATE"))
 			s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("極度認証(しなさい) %s", url))
 
+		}
+	}
+
+	// Start Spotify
+	if spotifyClient == nil {
+		s.MessageReactionAdd(m.ChannelID, m.ID, REACTION_FAILED)
+		return
+	}
+
+	if strings.HasPrefix(cmd, "play") {
+		if len(args) == 1 {
+			err := spotifyClient.Play()
+			if err != nil {
+				log.Println(err)
+				s.MessageReactionAdd(m.ChannelID, m.ID, REACTION_FAILED)
+			} else {
+				s.MessageReactionAdd(m.ChannelID, m.ID, REACTION_SUCCEED)
+				s.ChannelMessageSend(m.ChannelID, "**🎵START PLAYING**")
+			}
+		} else {
+			res, err := spotifyClient.Search(args[1], spotify.SearchTypeTrack)
+			if err != nil {
+				log.Println(err)
+				s.MessageReactionAdd(m.ChannelID, m.ID, REACTION_FAILED)
+			}
+			log.Println(res.Tracks.Tracks[0])
+			opt := &spotify.PlayOptions{
+				DeviceID: nil,
+			}
+
+			err = spotifyClient.QueueSongOpt(res.Tracks.Tracks[0].ID, opt)
+			if err != nil {
+				log.Println(err)
+				s.MessageReactionAdd(m.ChannelID, m.ID, REACTION_FAILED)
+			} else {
+				s.MessageReactionAdd(m.ChannelID, m.ID, REACTION_SUCCEED)
+				s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("**💿UPCOMING TRACK:** %s(%s)", res.Tracks.Tracks[0].Name, res.Tracks.Tracks[0].Album.Name))
+			}
+		}
+	}
+
+	if strings.HasPrefix(cmd, "skip") {
+		err := spotifyClient.Next()
+		if err != nil {
+			log.Println(err)
+			s.MessageReactionAdd(m.ChannelID, m.ID, REACTION_FAILED)
+		} else {
+			s.MessageReactionAdd(m.ChannelID, m.ID, REACTION_SUCCEED)
+			s.ChannelMessageSend(m.ChannelID, "**⏭️SKIPPED**")
 		}
 	}
 }
